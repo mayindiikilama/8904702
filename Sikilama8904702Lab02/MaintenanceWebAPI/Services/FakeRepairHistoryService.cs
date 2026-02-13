4cbe08ff683e2c41781a3fe1d15debc03368595e
Samson Ikilama <mayindiikilama@gmail.com>
2026-02-12 20:22:03 -0500

    Creation of fake application service

---
+++ Sikilama8904702Lab02/MaintenanceWebAPI/Services/FakeRepairHistoryService.cs
@@ -0,0 +1,33 @@
+﻿using MaintenanceWebAPI.DTO;
+
+namespace MaintenanceWebAPI.Services
+{
+    public class FakeRepairHistoryService
+    {
+        public List<RepairHistoryDto> GetByVehicleId(int vehicleId)
+        {
+            return new List<RepairHistoryDto>
+            {
+                new RepairHistoryDto
+                {
+                    Id = 1,
+                    VehicleId = vehicleId,
+                    RepairDate = DateTime.Now.AddDays(-10),
+                    Description = "Oil change",
+                    Cost = 89.99m,
+                    PerformedBy = "Quick Lube"
+                },
+                new RepairHistoryDto
+                {
+                    Id = 2,
+                    VehicleId = vehicleId,
+                    RepairDate = DateTime.Now.AddDays(-40),
+                    Description = "Brake pad replacement",
+                    Cost = 350.00m,
+                    PerformedBy = "Auto Repair Pro"
+                }
+            };
+        }
+
+    }
+}
