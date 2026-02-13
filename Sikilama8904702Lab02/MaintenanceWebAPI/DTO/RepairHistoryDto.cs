4bd99f7a93de755fbf45d82535ea159aafcf9403
Samson Ikilama <mayindiikilama@gmail.com>
2026-02-12 20:15:36 -0500

    creation of RepairHistoryDto

---
+++ Sikilama8904702Lab02/MaintenanceWebAPI/DTO/RepairHistoryDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MaintenanceWebAPI.DTO
+{
+    public class RepairHistoryDto
+    {
+        public int Id { get; set; }
+        public int VehicleId { get; set; }
+        public DateTime RepairDate { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Cost { get; set; }
+        public string PerformedBy { get; set; } = string.Empty;
+    }
+}
