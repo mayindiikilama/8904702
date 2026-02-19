b16f13a81dde40ffe94aca0c0f27699620c5833d
Samson Ikilama <mayindiikilama@gmail.com>
2026-02-19 14:46:32 -0500

    Creation of repair history view model

---
+++ Sikilama8904702Lab02/Week3MicroService/Models/RepairHistoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Week3MicroService.Models
+{
+    public class RepairHistoryViewModel
+    {
+        public int VehicleId { get; set; }
+        public DateTime RepairDate { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Cost { get; set; }
+        public string PerformedBy { get; set; } = string.Empty;
+    }
+}
