8a57221d499c2cc3e785adff55b2b6b64f70d252
Samson Ikilama <mayindiikilama@gmail.com>
2026-02-19 14:50:08 -0500

    Creation of Maintenance Controller

---
+++ Sikilama8904702Lab02/Week3MicroService/Controllers/MaintenanceController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Week3MicroService.Models;
+
+namespace Week3MicroService.Controllers
+{
+    public class MaintenanceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MaintenanceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult History()
+        {
+            return View(new List<RepairHistoryViewModel>());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> History(int vehicleId)
+        {
+            var client = _httpClientFactory.CreateClient("MaintenanceApi");
+            var repairs = await client.GetFromJsonAsync<List<RepairHistoryViewModel>>(
+                $"api/maintenance/vehicles/{vehicleId}/repairs");
+            return View(repairs ?? new List<RepairHistoryViewModel>());
+        }
+    }
+}
